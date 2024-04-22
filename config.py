43799838bef92cc71adafff62a66ebfcda27796c
CptDusty <125547469+CptDusty@users.noreply.github.com>
2024-04-22 14:09:05 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+from aiogram import Bot
+test_token = '5875035282:AAG8crtyf_30KvycQZJkPm5-vEwJA89zBHw'
+prod_token = "7060845514:AAEGMsFUhfXDCwrYkE1oeyjA87XBZfs17ug"
+Token = test_token
+bot = Bot(token=Token)
